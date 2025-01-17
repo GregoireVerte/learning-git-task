27204e399bf105dc0878c5ef90dab8cb8994de6d
Grzegorz Zielinski <trevor84@wp.pl>
2025-01-17 01:31:08 +0100

    Add_loop_and_printedlist

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ shopping_dict = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+total_items = 0
+
+print(f"Lista zakupów")
+for shop, items in shopping_dict.items():
+    shop = shop.capitalize()
+    items_capitalized = [item.capitalize() for item in items]
+    print(f"Idę do {shop}, kupuję tu następujące reczy: {items_capitalized}.")
+    
